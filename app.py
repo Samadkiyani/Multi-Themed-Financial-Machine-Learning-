0b4cba1c3f77f76923ba36b2141ed11e2b43bd3c
Abdul Samad Saleem <91059406+Samadkiyani@users.noreply.github.com>
2025-05-11 19:15:20 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib
 import plotly.express as px
 import plotly.graph_objects as go
 from sklearn.linear_model import LinearRegression
