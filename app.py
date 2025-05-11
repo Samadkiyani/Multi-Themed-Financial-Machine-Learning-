59f0f55cc9f9f3de561a98ff02bf59629453f150
Abdul Samad Saleem <91059406+Samadkiyani@users.noreply.github.com>
2025-05-11 21:46:14 +0500

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from sklearn.metrics import mean_squared_error, r2_score
 
 # Configure page
 st.set_page_config(
-    page_title="ML PRO MADE BY SAMAD KIANI",
+    page_title="ML PRO MADE BY Samad,Izza and Hashir",
     page_icon="🔥",
     layout="wide",
     initial_sidebar_state="expanded"
